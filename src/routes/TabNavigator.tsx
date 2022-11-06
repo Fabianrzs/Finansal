c824afdd916894503c28f4ec482d62984d5189d5
DarkarBlays <juanpamendoza08@gmail.com>
2022-11-06 12:50:16 -0500

    Abortos xd

--- src/routes/TabNavigator.tsx
+++ src/routes/TabNavigator.tsx
@@ -3,12 +3,14 @@ import HomeScreen from "../screens/home/homeScreen";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import Informe from "../screens/aplication/informe";
 import Registro from "../screens/aplication/registro";
-import { Text } from "react-native";
+import { Text, Image, View} from "react-native";
 import { StackScreenProps } from "@react-navigation/stack";
 import { darkBlack, lightWhite, secondaryWhite } from "../themes/_varibles";
 import { AuthContext } from "../context/AuthContext";
 import Button from "../components/common/Button";
 import FadeWelcome from "../components/fadeWelcome";
+
+
 
 const Tab = createBottomTabNavigator();
 
@@ -45,7 +47,25 @@ export default function TabNavigator({navigation}:Props){
   
   return(
     <Tab.Navigator>
-      <Tab.Screen name="Home" component={HomeScreen} />
+      <Tab.Screen name="Home" component={HomeScreen} 
+        options={{
+          tabBarIcon:({focused})=>(
+            <View>
+            <Image
+              source={require('../assets/icons/home.png')}
+              resizeMode='contain'
+              style={{
+                width: 25,
+                height: 25,
+                tintColor: focused? '#e32f45':'#748c94',
+              }}
+            />
+            <Text style={{color: focused? '#e32f45':'#748c94', fontSize: 12}}
+            >HOME</Text>
+            </View>
+          ),
+        }}
+      />
       <Tab.Screen name="Informe" component={Informe} />
       <Tab.Screen name="Registros" component={Registro} />
     </Tab.Navigator>
