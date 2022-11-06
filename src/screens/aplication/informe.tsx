c788bbf5d4dc95bfacee4a0ec3524991ac3535d2
Fdamirez <fabianramirez0072@gmail.com>
2022-11-05 22:37:40 -0500

    feat graphic

--- src/screens/aplication/informe.tsx
+++ src/screens/aplication/informe.tsx
@@ -1,10 +1,74 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { Dimensions, Text, View } from "react-native";
+import {
+  LineChart,
+  BarChart,
+  PieChart,
+  ProgressChart,
+  ContributionGraph,
+  StackedBarChart
+} from "react-native-chart-kit";
 
 export default function Informe(){
+  
+  const chartConfig = {
+    
+    backgroundGradientFrom: "#bf00ff",
+    backgroundGradientFromOpacity: 0,
+    backgroundGradientTo: "#0b00a2",
+    backgroundGradientToOpacity: 0.5,
+    color: (opacity = 1) => `rgba(26, 255, 146, ${opacity})`,
+    strokeWidth: 3, // optional, default 3
+    barPercentage: 0.5,
+    useShadowColorFromDataset: false // optional
+  };
+  
+  const data = [
+    {
+      name: "Seoul",
+      population: 21500000,
+      color: "rgb(0,98,255)",
+      legendFontColor: "#7F7F7F",
+      legendFontSize: 15
+    },
+    {
+      name: "Toronto",
+      population: 2800000,
+      color: "#F00",
+      legendFontColor: "#7F7F7F",
+      legendFontSize: 15
+    },
+    {
+      name: "New York",
+      population: 8538000,
+      color: "#00ff3c",
+      legendFontColor: "#7F7F7F",
+      legendFontSize: 15
+    },
+    {
+      name: "Moscow",
+      population: 11920000,
+      color: "rgb(0, 0, 255)",
+      legendFontColor: "#7F7F7F",
+      legendFontSize: 15
+    }
+  ];
+  
+  const screenWidth = Dimensions.get("screen").width
+  
   return(
-    <View style={{flex:1, justifyContent: 'center'}}>
-      <Text style={{fontWeight:'bold'}}> Informes </Text>
+    <View>
+      <PieChart
+        data={data}
+        width={screenWidth}
+        height={200}
+        chartConfig={chartConfig}
+        accessor={"population"}
+        backgroundColor={"transparent"}
+        paddingLeft={"0"}
+        center={[10, 10]}
+        absolute
+      />
     </View>
   )
 }
